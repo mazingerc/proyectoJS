57781aa6b9b6ff3d33585db6655683a36003be89
mazingerc <mazingercesar@gmail.com>
2023-03-21 19:18:46 +0100

    corrección primera pre-entrega js

--- main.js
+++ main.js
@@ -1,28 +1,30 @@
-alert('Veamos cual es tu número de la suerte')
-let edad = parseInt(prompt("Dime tu edad"));
-if(edad >=18){
-    alert('Perfecto! sigamos');
-}else{
-    alert('Según la ley XXXX no puedes... es broma, A JUGAR!');
-}
+setTimeout(()=>{
+    alert('Veamos cual es tu número de la suerte')
+    let edad = parseInt(prompt("Dime tu edad"));
+    if(edad >=18){
+        alert('Perfecto! sigamos');
+    }else{
+        alert('Según la ley XXXX no puedes... es broma, A JUGAR!');
+    }
 
-let nombreUsuario = prompt('¿Cúal es tu nombre?');
-while(nombreUsuario==''){
-   alert('Disculpa, ¿puedes repetir?');
-   nombreUsuario = prompt('¿Cúal es tu nombre?');
-}
-alert('Hola '+nombreUsuario+ ', ahora dime tres años importantes para ti');
+    let nombreUsuario = prompt('¿Cúal es tu nombre?');
+    while(nombreUsuario==''){
+    alert('Disculpa, ¿puedes repetir?');
+    nombreUsuario = prompt('¿Cúal es tu nombre?');
+    }
+    alert('Hola '+nombreUsuario+ ', ahora dime tres años importantes para ti');
 
-let acumulador = 0;
-for(let i=1; i<4; i++){
-    let fecha = parseInt(prompt('Introduce la fecha número '+i));
-    acumulador = acumulador + fecha;
-}
+    let acumulador = 0;
+    for(let i=1; i<4; i++){
+        let fecha = parseInt(prompt('Introduce la fecha número '+i));
+        acumulador = acumulador + fecha;
+    }
 
-function numeroFinal(){
-    let calculo = parseInt(acumulador / 3 + acumulador);
-    alert('El número al que debes apostar en la Quiniela es: '+calculo);
-}
+    function numeroFinal(){
+        let calculo = parseInt(acumulador / 3 + acumulador);
+        alert('El número al que debes apostar en la Quiniela es: '+calculo);
+    }
 
-numeroFinal();
-alert('Mucha suerte y hasta la próxima');
+    numeroFinal();
+    alert('Mucha suerte y hasta la próxima');
+},1000);
