b90f3a96d18f46193ad313ef3a7ab29694b4c779
mazingerc <mazingercesar@gmail.com>
2023-04-20 20:04:16 +0200

    tercera pre-entrega js

--- main.js
+++ main.js
@@ -1,53 +1,65 @@
-setTimeout(()=>{
-
-    alert('Veamos cuales son tus números de la suerte')
-    let edad = parseInt(prompt("Dime tu edad"));
-        if(edad >=18){
-    alert('Perfecto! sigamos');
-    }   else{
-    alert('Debes ser mayor de edad para jugar pero haremos una excepción');
+function generateNumbers() {
+    //Aleatorio bolas azules
+    let numbers = [];
+    while (numbers.length < 5) {
+      let randomNum = Math.floor(Math.random() * 50) + 1;
+      if (!numbers.includes(randomNum)) {
+        numbers.push(randomNum);
+      }
     }
-
-    let nombreUsuario = prompt('¿Cúal es tu nombre?');
-        while(nombreUsuario==''){
-    alert('Disculpa, ¿puedes repetir?');
-    nombreUsuario = prompt('¿Cúal es tu nombre?');
+    //Aleatorio bolas amarillas
+    let luckyStars = [];
+    while (luckyStars.length < 2) {
+      let randomNum = Math.floor(Math.random() * 12) + 1;
+      if (!luckyStars.includes(randomNum)) {
+        luckyStars.push(randomNum);
+      }
     }
-    alert('Hola '+nombreUsuario+ ', te daré uno a uno los números de tu combinación ganadora');
-
-    function generadorAleatorio(min, max){
-        return Math.floor(Math.random() * (max - min + 1) + min);
+  
+    //Mostrar los números aleatorios en las bolas
+    let numbersContainer = document.querySelector(".numbers");
+    numbersContainer.innerHTML = "";
+    for (let i = 0; i < numbers.length; i++) {
+      let ball = document.createElement("div");
+      ball.classList.add("ball");
+      ball.textContent = numbers[i];
+      numbersContainer.appendChild(ball);
     }
-        for(let i = 1; i <= 5; ++i){
-    alert(generadorAleatorio(1,50));
+  
+    let luckyStarsContainer = document.querySelector(".lucky-stars");
+    luckyStarsContainer.innerHTML = "";
+    for (let i = 0; i < luckyStars.length; i++) {
+      let ball = document.createElement("div");
+      ball.classList.add("ball");
+      ball.textContent = luckyStars[i];
+      luckyStarsContainer.appendChild(ball);
     }
-
-    let respuesta = true;
-    for (respuesta; respuesta == true;){
-    consulta = prompt('¿Quiere consultar los premios? \n 1. Acierto \n 2. Aciertos \n 3. Aciertos \n 4. Aciertos \n 5. Aciertos \n 6. No');
-    
-        if(consulta == 1)
-            alert('Has ganado ' +premios[0]+ ' euros');
-        else if(consulta == 2)
-            alert('Bien! Has ganado ' +premios[1]+ ' euros');
-        else if(consulta == 3)
-            alert('Muy bien! Has ganado ' +premios[2]+ ' euros');
-        else if(consulta == 4)
-            alert('Genial! Has ganado ' +premios[3]+ ' euros');
-        else if(consulta == 5)
-            alert('Felicidades! Has ganado ' +premios[4]+ ' euros');
-        else(consulta == 6)
-            alert('Recuerda que el próximo sorteo se celebra el ' +sorteos.fecha)
-            alert('Muchas gracias, nos vemos pronto!');
-        break;
+  
+    //Mantener los datos del usuario en localStorage
+    let name = document.getElementById("name").value;
+    let email = document.getElementById("email").value;
+    let age = document.getElementById("age").value;
+    let userData = {
+      name: name,
+      email: email,
+      age: age,
+    };
+    localStorage.setItem("userData", JSON.stringify(userData));
+  }
+  
+  //Función del botón "Mucha suerte"
+  function handleClick() {
+    //Validar la edad del usuario
+    let age = document.getElementById("age").value;
+    if (age < 18) {
+      alert("Espera a tener 18 años para participar");
+      return;
     }
-
-
-},1000);
-
-const premios = [5, 20, 500, 2000, 5000]
-const sorteos = {
-    nombre: "euromillon",
-    fecha: "jueves",
-    bote: 10000,
-}
+  
+    //Generar los números aleatorios y mostrarlos
+    generateNumbers();
+  }
+  
+  //Evento que se ejecuta al hacer clic en el botón "Mucha suerte"
+  let generateBtn = document.getElementById("generate-btn");
+  generateBtn.addEventListener("click", handleClick);
